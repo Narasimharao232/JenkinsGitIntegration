6dc9a545a7e2c73909ac71b71f13fa7524737896
Narasimharao232 <73817624+Narasimharao232@users.noreply.github.com>
2022-06-21 17:39:22 +0530

    Update sample.bat

--- sample.bat
+++ sample.bat
@@ -1,2 +1,2 @@
 echo "qwerty"
-
+https://iihtlimited-my.sharepoint.com/personal/muhammed_m_iiht_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Fmuhammed%5Fm%5Fiiht%5Fcom%2FDocuments%2FDelivery%2FFY%2022%2D23%2FJune%2FCGI%2FIIHTELSTECH31639%5FDevOps%2FTraining%20Materials%2FStudy%20Materials%2FDay2%5FJenkins
